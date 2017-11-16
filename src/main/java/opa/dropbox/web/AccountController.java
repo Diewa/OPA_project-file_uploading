ae220e6e6f9f66c31e6ac0fb39d460c69bc40dc6
Diewa <urbanowsky.d@gmail.com>
2017-11-16 22:38:25 +0100

    remove gradle project, init rails project

--- src/main/java/opa/dropbox/web/AccountController.java
+++
@@ -1,4 +0,0 @@
-package opa.dropbox.web;
-
-public class AccountController {
-}
--- src/main/java/opa/dropbox/web/RequestPath.java
+++
@@ -1,8 +0,0 @@
-package opa.dropbox.web;
-
-public final class RequestPath {
-
-    public static final String USER_PATH = "/users";
-
-    public static final String USER_FILE_PATH = USER_PATH + "/{userId:.+}/file";
-}
